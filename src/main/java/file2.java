d5dddc059772b08c112b18c1292aaeafa2e50ce9
Student <student@b426-2.intranet.cs.upt.ro>
2026-05-27 16:23:32 +0300

    added file

---
+++ src/main/java/file2.java
@@ -0,0 +1,9 @@
+package loose.oose.fis.lab.student.manager.model;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+}
+
